5d379af8e04b0b517cff787431a2f5efd1fa1fd9
sandecodes <muhammadsandy74@gmail.com>
2021-12-16 19:31:01 +0700

    first commit

--- 8 kyu/calculate-bmi.js
+++ 8 kyu/calculate-bmi.js
@@ -1,3 +1,4 @@
 bmi = (weight, height, bmi = weight / (height * height)) => (bmi <= 18.5 ? 'Underweight' : bmi <= 25 ? 'Normal' : bmi <= 30 ? 'Overweight' : 'Obese');
 
 console.log(bmi(80, 1.8));
+console.log(bmi(70, 1.8));
