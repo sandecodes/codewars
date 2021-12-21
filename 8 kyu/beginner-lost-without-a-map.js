a79cd9662f51e433b805eb22cba6a58b2edbe722
sandecodes <muhammadsandy74@gmail.com>
2021-12-21 15:20:24 +0700

    first commit

---
+++ 8 kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,11 @@
+/*
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+*/
+
+maps = (arrays) => arrays.map((array) => array * 2);
