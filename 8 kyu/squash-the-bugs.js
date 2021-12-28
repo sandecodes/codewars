55d0c013b45f0def582833db11629dd96cc77380
sandecodes <muhammadsandy74@gmail.com>
2021-12-28 10:23:37 +0700

    first commit

--- 8 kyu/squash-the-bugs.js
+++ 8 kyu/squash-the-bugs.js
@@ -1,17 +1,31 @@
-function findLongest(str) {
-  var spl = str.split(' ');
-  var longest = 0;
+/*
 
-  for (var i = 0; i > spl.length; i++) {
-    if (spl(i).length > longest) {
-      longest = spl[i].length;
+Description:
+
+Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+There will only be one 'longest' word.
+
+*/
+
+// Optional 1
+
+function findLongest(str) {
+  let s = str.split(' ');
+  let longest = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].length > longest) {
+      longest = s[i].length;
     }
   }
   return longest;
 }
 
-console.log(findLongest('The quick white fox jumped around the massive dog'), 7);
-console.log(findLongest('Take me to tinseltown with you'), 10);
-console.log(findLongest('Sausage chops'), 7);
-console.log(findLongest('Wind your body and wiggle your belly'), 6);
-console.log(findLongest('Lets all go on holiday'), 7);
+/*
+
+Optional 2
+
+const findLongest = (str) => Math.max(...str.split(' ').map((word) => word.length));
+
+*/
