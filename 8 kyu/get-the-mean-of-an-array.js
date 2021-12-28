ea0655967174f7a993bc269e8cd2763c9c6bb979
sandecodes <muhammadsandy74@gmail.com>
2021-12-28 11:23:17 +0700

    first commit

--- 8 kyu/get-the-mean-of-an-array.js
+++ 8 kyu/get-the-mean-of-an-array.js
@@ -9,7 +9,3 @@ The array will never be empty.
 */
 
 getAverage = (marks) => Math.floor(marks.reduce((a, b) => a + b) / marks.length);
-
-console.log(getAverage([2, 2, 2, 2]), 2);
-console.log(getAverage([1, 2, 3, 4, 5]), 3);
-console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]), 1);
