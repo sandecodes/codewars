3cbe81bcd196e090012309cb9900021836719334
sandecodes <muhammadsandy74@gmail.com>
2021-12-30 11:18:43 +0700

    first commit

---
+++ 8 kyu/fundamentals-return.js
@@ -0,0 +1,35 @@
+/*
+
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+
+a (operation) b*
+
+*/
+
+add = (a, b) => a + b;
+
+divide = (a, b) => a / b;
+
+multiply = (a, b) => a * b;
+
+mod = (a, b) => a % b;
+
+exponent = (a, b) => Math.pow(a, b);
+
+subt = (a, b) => a - b;
