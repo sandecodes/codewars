150c55677bcde5ffa494677d69a430e069893422
sandecodes <muhammadsandy74@gmail.com>
2022-01-04 11:12:00 +0700

    first commit

---
+++ 8 kyu/name-shuffler.js
@@ -0,0 +1,11 @@
+/*
+
+Description:
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"
+
+*/
+
+nameShuffler = (str) => str.split(' ').reverse().join(' ');
