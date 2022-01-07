1717ccfc731133008ce0d3c7585f352f34618eb3
sandecodes <muhammadsandy74@gmail.com>
2022-01-07 10:30:33 +0700

    first commit

--- 8 kyu/do-i-get-a-bonus.js
+++ 8 kyu/do-i-get-a-bonus.js
@@ -17,11 +17,3 @@ function bonusTime(salary, bonus) {
 
 /* Optional - 2 */
 bonusTime = (salary, bonus) => (bonus === true ? '£' + salary * 10 : '£' + salary);
-
-console.log(bonusTime(10000, true), '£100000');
-console.log(bonusTime(25000, true), '£250000');
-console.log(bonusTime(10000, false), '£10000');
-console.log(bonusTime(60000, false), '£60000');
-console.log(bonusTime(2, true), '£20');
-console.log(bonusTime(78, false), '£78');
-console.log(bonusTime(67890, true), '£678900');
